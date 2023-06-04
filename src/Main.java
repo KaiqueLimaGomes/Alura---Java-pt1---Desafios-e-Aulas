50a6284f5992cc880067ab302aa20b1220e018a7
Kaique <kaiquelimag@gmail.com>
2023-06-04 20:47:04 -0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Movies List");
+        System.out.println("Filme: Top Gun: Maverick");
 
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double media = 7.8 + 6.5 + 9.3/3;
+        double notaDoFilme = 8.1;
+        String sinopse = "Filme de aventura com galã dis anos 80";
+
+        int classificacao = (int) (media/2);
+        System.out.println(media);
+        System.out.println(classificacao);
 
     }
 }
