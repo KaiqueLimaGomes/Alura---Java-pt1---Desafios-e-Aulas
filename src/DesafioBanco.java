c3345036206926d6b9595979ee21a0c7ea96f61d
Kaique <kaiquelimag@gmail.com>
2023-06-11 11:10:01 -0300

    Finalizado_DesafioBanco
    Desafio Finalizado.

--- src/DesafioBanco.java
+++ src/DesafioBanco.java
@@ -5,6 +5,9 @@ public class DesafioBanco {
         Scanner inputTeclado = new Scanner(System.in);
 
         String nome;
+        double saldo = 1320.00;
+        double valor = 0;
+        int opcao = 0;
 
         System.out.printf("""
                 Olá, seja bem-vindo ao Banco Desafio!
@@ -12,8 +15,78 @@ public class DesafioBanco {
                 """);
         System.out.printf("Digite seu nome completo: ");
          nome = inputTeclado.nextLine();
-        System.out.println();
+        System.out.printf("""
+                \n
+                **************************************************
+                    Seja Bem-vindo %s ao Banco Desafio!
+                **************************************************
+                
+                Dados do cliente:
+                
+                Nome:           %s.
+                Tipo de Conta:  Corrente.
+                Saldo Inicial:  %.2f
+                
+                """, nome, nome, saldo);
 
+        while (opcao != 4){
+            System.out.printf("""
+                   -------------------------------------------      
+                    
+                    Operações
+                    
+                    1- Consultar Saldo
+                    2- Receber Valor
+                    3- Transferir Valor
+                    4- Sair
+                    
+                    """);
+            System.out.printf("Digite a opção desejada: ");
+            opcao = inputTeclado.nextInt();
+            System.out.println("");
 
+            switch (opcao){
+
+                default:
+                    System.out.println("\n Opção inválida, Digite Novamente!");
+                    break;
+
+                case 1 :
+                    System.out.printf("Olá %s, atualmente seu saldo é de R$ %.2f. \n", nome, saldo);
+                    break;
+
+                case 2:
+                    System.out.printf("""
+                            **** Menu - Receber Valor ****
+                            
+                            Digite o valor a ser recebido: R$   """);
+                    saldo += inputTeclado.nextDouble();
+                    ;
+                    System.out.println("Valor recebido com sucesso!");
+                    break;
+
+                case 3:
+                    System.out.printf("""
+                            **** Menu - Transferir Valor ****
+                            
+                            Digite o valor a ser Transferido: R$    """);
+                    valor = inputTeclado.nextDouble();
+
+                    if(valor > saldo){
+                        System.out.println("Saldo insuficiente!");
+                    }else{
+                        saldo -= valor;
+                        System.out.println("Valor transferido com sucesso!");
+                        System.out.println("***************************** \n");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Obrigado por utilizar o Banco Desafio, até a próxima!");
+                    opcao = 4;
+
+                    break;
+            }
+        }
     }
 }
